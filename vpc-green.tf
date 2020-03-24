c3e3a483bafdfe493837ee62700328bab506a89f
Sam Bryant <srbry@hotmail.com>
2020-03-24 10:54:03 +0000

    Tidy up terraform

--- vpc-green.tf
+++ vpc-green.tf
@@ -2,10 +2,8 @@
 #### VPC ROUTING ETC #####
 ##########################
 
-# transit 1 is from primary too secandry
-
 resource "aws_vpc" "poc-vpc-transit-green" {
-  cidr_block = "192.168.253.0/24"
+  cidr_block = "192.168.240.0/23"
 
   tags = {
     Name = "poc-vpc-transit-green"
@@ -13,13 +11,15 @@ resource "aws_vpc" "poc-vpc-transit-green" {
 }
 
 resource "aws_subnet" "poc-vpc-transit-green" {
-  availability_zone       = data.aws_availability_zones.available.names[0]
-  cidr_block              = "192.168.253.0/24"
+  count = 2
+
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  cidr_block              = cidrsubnet(aws_vpc.poc-vpc-transit-green.cidr_block, 1, count.index)
   vpc_id                  = aws_vpc.poc-vpc-transit-green.id
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "poc-vpc-transit-green"
+    Name = "poc-vpc-transit-green-${count.index}"
   }
 
   depends_on = [aws_internet_gateway.poc-vpc-transit-green]
@@ -47,7 +47,9 @@ resource "aws_route_table" "poc-vpc-transit-green" {
 }
 
 resource "aws_route_table_association" "poc-vpc-transit-green" {
-  subnet_id      = aws_subnet.poc-vpc-transit-green.id
+  count = length(aws_subnet.poc-vpc-transit-green)
+
+  subnet_id      = aws_subnet.poc-vpc-transit-green[count.index].id
   route_table_id = aws_route_table.poc-vpc-transit-green.id
 }
 
@@ -65,7 +67,7 @@ resource "aws_ec2_transit_gateway_route_table" "poc-vpc-transit-green" {
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "poc-vpc-transit-green" {
-  subnet_ids                                      = [aws_subnet.poc-vpc-transit-green.id]
+  subnet_ids                                      = aws_subnet.poc-vpc-transit-green.*.id
   transit_gateway_id                              = aws_ec2_transit_gateway.poc-vpc-transit-gateway.id
   vpc_id                                          = aws_vpc.poc-vpc-transit-green.id
   transit_gateway_default_route_table_association = false
@@ -90,26 +92,14 @@ resource "aws_ec2_transit_gateway_route_table_association" "poc-vpc-transit-gree
 #### nat instance    #####
 ##########################
 
+resource "aws_eip" "poc-vpc-transit-green" {
+  count = length(aws_subnet.poc-vpc-transit-green)
 
-resource "aws_eip" "poc-vpc-transit-green-primary" {
-  vpc = true
-
-  network_interface = aws_network_interface.poc-vpc-transit-green-primary.id
-
-  tags = {
-    Name = "poc-vpc-transit-green-primary"
-  }
-
-  depends_on = [aws_internet_gateway.poc-vpc-transit-green]
-}
-
-resource "aws_eip" "poc-vpc-transit-green-secondary" {
-  vpc = true
-
-  network_interface = aws_network_interface.poc-vpc-transit-green-secondary.id
+  vpc               = true
+  network_interface = aws_network_interface.poc-vpc-transit-green[count.index].id
 
   tags = {
-    Name = "poc-vpc-transit-green-secondary"
+    Name = "poc-vpc-transit-green-${count.index}"
   }
 
   depends_on = [aws_internet_gateway.poc-vpc-transit-green]
@@ -138,96 +128,42 @@ resource "aws_security_group" "poc-vpc-transit-green" {
   }
 }
 
-resource "aws_network_interface" "poc-vpc-transit-green-primary" {
-  subnet_id         = aws_subnet.poc-vpc-transit-green.id
-  private_ips       = ["192.168.253.11"]
+resource "aws_network_interface" "poc-vpc-transit-green" {
+  count = length(aws_subnet.poc-vpc-transit-green)
+
+  subnet_id         = aws_subnet.poc-vpc-transit-green[count.index].id
+  private_ips       = [cidrhost(aws_subnet.poc-vpc-transit-green[count.index].cidr_block, 11)]
   security_groups   = [aws_security_group.poc-vpc-transit-green.id]
   source_dest_check = false
 
   tags = {
-    Name = "poc-vpc-transit-green-primary"
+    Name = "poc-vpc-transit-green-${count.index}"
   }
 }
 
-resource "aws_network_interface" "poc-vpc-transit-green-secondary" {
-  subnet_id         = aws_subnet.poc-vpc-transit-green.id
-  private_ips       = ["192.168.253.12"]
-  security_groups   = [aws_security_group.poc-vpc-transit-green.id]
-  source_dest_check = false
+resource "aws_instance" "poc-vpc-green-nat-gateway" {
+  count = length(aws_subnet.poc-vpc-transit-green)
 
-  tags = {
-    Name = "poc-vpc-transit-green-secondary"
-  }
-}
-
-resource "aws_instance" "poc-vpc-green-nat-gateway-primary" {
   ami                  = data.aws_ami.amazon-linux.id
   instance_type        = "m4.large"
   key_name             = aws_key_pair.poc-vpc-transit.key_name
   iam_instance_profile = aws_iam_instance_profile.poc-vpc-transit-nat.name
 
   network_interface {
-    network_interface_id = aws_network_interface.poc-vpc-transit-green-primary.id
+    network_interface_id = aws_network_interface.poc-vpc-transit-green[count.index].id
     device_index         = 0
   }
 
   tags = {
-    Name = "NATPrimary"
-    Use  = "poc-vpc-transit-green-nat-primary"
-  }
-
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = tls_private_key.poc-vpc-transit.private_key_pem
-    host        = aws_eip.poc-vpc-transit-green-primary.public_ip
-  }
-
-  provisioner "file" {
-    source      = "config/health_monitor.sh"
-    destination = "/home/ec2-user/health_monitor.sh"
-  }
-
-  provisioner "file" {
-    source      = "config/tgw_monitor.sh"
-    destination = "/home/ec2-user/tgw_monitor.sh"
-  }
-
-  provisioner "file" {
-    source      = "config/setup.sh"
-    destination = "/home/ec2-user/setup.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod a+x setup.sh",
-      "./setup.sh"
-    ]
-  }
-}
-
-resource "aws_instance" "poc-vpc-green-nat-gateway-secondary" {
-  ami                  = data.aws_ami.amazon-linux.id
-  instance_type        = "m4.large"
-  key_name             = aws_key_pair.poc-vpc-transit.key_name
-  iam_instance_profile = aws_iam_instance_profile.poc-vpc-transit-nat.name
-
-  network_interface {
-    network_interface_id = aws_network_interface.poc-vpc-transit-green-secondary.id
-    device_index         = 0
-  }
-
-  tags = {
-    Name = "NATSecondary"
-    Use  = "poc-vpc-transit-green-nat-secondary"
+    Name = count.index == 0 ? "NATPrimary" : "NATSecondary"
+    Use  = count.index == 0 ? "poc-vpc-transit-green-nat-primary" : "poc-vpc-transit-green-nat-secondary"
   }
 
   connection {
     type        = "ssh"
     user        = "ec2-user"
     private_key = tls_private_key.poc-vpc-transit.private_key_pem
-    host        = aws_eip.poc-vpc-transit-green-secondary.public_ip
+    host        = aws_eip.poc-vpc-transit-green[count.index].public_ip
   }
 
   provisioner "file" {
--- vpc-red.tf
+++ vpc-red.tf
@@ -2,10 +2,8 @@
 #### VPC ROUTING ETC #####
 ##########################
 
-# transit 1 is from primary too secandry
-
 resource "aws_vpc" "poc-vpc-transit-red" {
-  cidr_block = "192.168.252.0/24"
+  cidr_block = "192.168.250.0/23"
 
   tags = {
     Name = "poc-vpc-transit-red"
@@ -13,13 +11,15 @@ resource "aws_vpc" "poc-vpc-transit-red" {
 }
 
 resource "aws_subnet" "poc-vpc-transit-red" {
-  availability_zone       = data.aws_availability_zones.available.names[0]
-  cidr_block              = "192.168.252.0/24"
+  count = 2
+
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  cidr_block              = cidrsubnet(aws_vpc.poc-vpc-transit-red.cidr_block, 1, count.index)
   vpc_id                  = aws_vpc.poc-vpc-transit-red.id
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "poc-vpc-transit-red"
+    Name = "poc-vpc-transit-red-${count.index}"
   }
 
   depends_on = [aws_internet_gateway.poc-vpc-transit-red]
@@ -47,7 +47,9 @@ resource "aws_route_table" "poc-vpc-transit-red" {
 }
 
 resource "aws_route_table_association" "poc-vpc-transit-red" {
-  subnet_id      = aws_subnet.poc-vpc-transit-red.id
+  count = length(aws_subnet.poc-vpc-transit-green)
+
+  subnet_id      = aws_subnet.poc-vpc-transit-red[count.index].id
   route_table_id = aws_route_table.poc-vpc-transit-red.id
 }
 
@@ -65,7 +67,7 @@ resource "aws_ec2_transit_gateway_route_table" "poc-vpc-transit-red" {
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "poc-vpc-transit-red" {
-  subnet_ids                                      = [aws_subnet.poc-vpc-transit-red.id]
+  subnet_ids                                      = aws_subnet.poc-vpc-transit-red.*.id
   transit_gateway_id                              = aws_ec2_transit_gateway.poc-vpc-transit-gateway.id
   vpc_id                                          = aws_vpc.poc-vpc-transit-red.id
   transit_gateway_default_route_table_association = false
@@ -91,25 +93,14 @@ resource "aws_ec2_transit_gateway_route_table_association" "poc-vpc-transit-red"
 ##########################
 
 
-resource "aws_eip" "poc-vpc-transit-red-primary" {
-  vpc = true
+resource "aws_eip" "poc-vpc-transit-red" {
+  count = length(aws_subnet.poc-vpc-transit-green)
 
-  network_interface = aws_network_interface.poc-vpc-transit-red-primary.id
-
-  tags = {
-    Name = "poc-vpc-transit-red-primary"
-  }
-
-  depends_on = [aws_internet_gateway.poc-vpc-transit-red]
-}
-
-resource "aws_eip" "poc-vpc-transit-red-secondary" {
-  vpc = true
-
-  network_interface = aws_network_interface.poc-vpc-transit-red-secondary.id
+  vpc               = true
+  network_interface = aws_network_interface.poc-vpc-transit-red[count.index].id
 
   tags = {
-    Name = "poc-vpc-transit-red-secondary"
+    Name = "poc-vpc-transit-red-${count.index}"
   }
 
   depends_on = [aws_internet_gateway.poc-vpc-transit-red]
@@ -138,95 +129,42 @@ resource "aws_security_group" "poc-vpc-transit-red" {
   }
 }
 
-resource "aws_network_interface" "poc-vpc-transit-red-primary" {
-  subnet_id         = aws_subnet.poc-vpc-transit-red.id
-  private_ips       = ["192.168.252.11"]
+resource "aws_network_interface" "poc-vpc-transit-red" {
+  count = length(aws_subnet.poc-vpc-transit-green)
+
+  subnet_id         = aws_subnet.poc-vpc-transit-red[count.index].id
+  private_ips       = [cidrhost(aws_subnet.poc-vpc-transit-red[count.index].cidr_block, 11)]
   security_groups   = [aws_security_group.poc-vpc-transit-red.id]
   source_dest_check = false
 
   tags = {
-    Name = "poc-vpc-transit-red-primary"
+    Name = "poc-vpc-transit-red-${count.index}"
   }
 }
 
-resource "aws_network_interface" "poc-vpc-transit-red-secondary" {
-  subnet_id         = aws_subnet.poc-vpc-transit-red.id
-  private_ips       = ["192.168.252.12"]
-  security_groups   = [aws_security_group.poc-vpc-transit-red.id]
-  source_dest_check = false
+resource "aws_instance" "poc-vpc-red-nat-gateway" {
+  count = length(aws_subnet.poc-vpc-transit-green)
 
-  tags = {
-    Name = "poc-vpc-transit-red-secondary"
-  }
-}
-
-resource "aws_instance" "poc-vpc-red-nat-gateway-primary" {
   ami                  = data.aws_ami.amazon-linux.id
   instance_type        = "m4.large"
   key_name             = aws_key_pair.poc-vpc-transit.key_name
   iam_instance_profile = aws_iam_instance_profile.poc-vpc-transit-nat.name
 
   network_interface {
-    network_interface_id = aws_network_interface.poc-vpc-transit-red-primary.id
+    network_interface_id = aws_network_interface.poc-vpc-transit-red[count.index].id
     device_index         = 0
   }
 
   tags = {
-    Name = "NATPrimary"
-    Use  = "poc-vpc-transit-red-nat-primary"
+    Name = count.index == 0 ? "NATPrimary" : "NATSecondary"
+    Use  = count.index == 0 ? "poc-vpc-transit-red-nat-primary" : "poc-vpc-transit-red-nat-secondary"
   }
 
   connection {
     type        = "ssh"
     user        = "ec2-user"
     private_key = tls_private_key.poc-vpc-transit.private_key_pem
-    host        = aws_eip.poc-vpc-transit-red-primary.public_ip
-  }
-
-  provisioner "file" {
-    source      = "config/health_monitor.sh"
-    destination = "/home/ec2-user/health_monitor.sh"
-  }
-
-  provisioner "file" {
-    source      = "config/tgw_monitor.sh"
-    destination = "/home/ec2-user/tgw_monitor.sh"
-  }
-
-  provisioner "file" {
-    source      = "config/setup.sh"
-    destination = "/home/ec2-user/setup.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod a+x setup.sh",
-      "./setup.sh"
-    ]
-  }
-}
-
-resource "aws_instance" "poc-vpc-red-nat-gateway-secondary" {
-  ami                  = data.aws_ami.amazon-linux.id
-  instance_type        = "m4.large"
-  key_name             = aws_key_pair.poc-vpc-transit.key_name
-  iam_instance_profile = aws_iam_instance_profile.poc-vpc-transit-nat.name
-
-  network_interface {
-    network_interface_id = aws_network_interface.poc-vpc-transit-red-secondary.id
-    device_index         = 0
-  }
-
-  tags = {
-    Name = "NATSecondary"
-    Use  = "poc-vpc-transit-red-nat-secondary"
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = tls_private_key.poc-vpc-transit.private_key_pem
-    host        = aws_eip.poc-vpc-transit-red-secondary.public_ip
+    host        = aws_eip.poc-vpc-transit-red[count.index].public_ip
   }
 
   provisioner "file" {
